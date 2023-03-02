374c8594edf4f4fe5f831d955e14b53353d01808
user6879 <bpoliar01@gmail.com>
2023-03-02 20:35:37 +0500

    another try

--- flood.rs
+++ flood.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main(){
 
-    const NUMBER_OF_COMMITS: i32 = 200;
+    const NUMBER_OF_COMMITS: i32 = 500;
 
     let mut count: i32 = 0;
 
@@ -12,11 +12,10 @@ fn main(){
 
         fs::File::create(format!("./files-rs/{}.txt", count.clone()));
         process::Command::new("git").arg("add").arg(format!("./files-rs/{}.txt", count.clone())).spawn().expect("Couldnt add").wait();
-        process::Command::new("git").arg("commit").arg("-m").arg(format!("\".{}\"", count.clone())).spawn().expect("Couldnt commit").wait();
+        process::Command::new("git").arg("commit").arg("-m").arg(format!("\"{}.txt\"", count.clone())).spawn().expect("Couldnt commit").wait();
 
         if count==NUMBER_OF_COMMITS{
             process::Command::new("git").arg("push").arg("origin").arg("main").spawn();
-
             break;
         }
     }
