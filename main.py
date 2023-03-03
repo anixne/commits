2ae77228fbf73003660c3da363d51da0d3b45d1b
user6879 <bpoliar01@gmail.com>
2023-03-03 18:12:50 +0500

    cross-interpretation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 NUMBER_OF_COMMITS = 500
 
 for i in range(NUMBER_OF_COMMITS):
-    os.system(f"touch ./files/{i}.txt")
+    open(f"touch ./files/{i}.txt", "w")
     os.system(f"git add ./files/{i}.txt")
     os.system(f'git commit -m "{i}.txt"')
 
